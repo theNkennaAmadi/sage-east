36c7bdfe512a89842b1174bb18879428cd5e2348
theNkennaAmadi <108593101+theNkennaAmadi@users.noreply.github.com>
2023-12-04 16:24:30 +0000

    init

--- global.js
+++ global.js
@@ -170,10 +170,16 @@ class TextAnimator {
 
 class GalleryScroller {
   constructor(container) {
-    this.iteration = 0;
+    this.initializeProperties(container);
+    this.setupScrollTrigger();
+    this.setupDraggable();
+    this.setupInitialAnimations();
+  }
 
-    this.spacing = 0.15; // spacing of the cards (stagger)
-    this.snapTime = gsap.utils.snap(this.spacing); // we'll use this to snapTime the playhead on the seamlessLoop
+  initializeProperties(container) {
+    this.iteration = 0;
+    this.spacing = 0.15;
+    this.snapTime = gsap.utils.snap(this.spacing);
     this.cards = [...container.querySelectorAll(".works-list-item")];
     this.cardsList = container.querySelector(".works-list");
     this.cardsListWrapper = container.querySelector(".works-list-wrapper");
@@ -184,24 +190,10 @@ class GalleryScroller {
       this.spacing,
       this.animateFunc.bind(this)
     );
-    this.playhead = { offset: 0 }; // a proxy object we use to simulate the playhead position
+    this.playhead = { offset: 0 };
+  }
 
-    // Set initial state of items
-    gsap.set(this.cards, { x: "400%", opacity: 0, scale: 0 });
-    gsap.from(this.sectionWrapper, {
-      autoAlpha: 0,
-      duration: 1,
-      ease: "expo",
-    });
-
-    this.scrub = gsap.to(this.playhead, {
-      offset: 0,
-      onUpdate: this.onScrubUpdate.bind(this),
-      duration: 0.5,
-      ease: "power3",
-      paused: true,
-    });
-
+  setupScrollTrigger() {
     this.trigger = ScrollTrigger.create({
       start: 0,
       onUpdate: this.onScrollUpdate.bind(this),
@@ -212,13 +204,28 @@ class GalleryScroller {
     ScrollTrigger.addEventListener("scrollEnd", () =>
       this.scrollToOffset(this.scrub.vars.offset)
     );
+  }
 
+  setupDraggable() {
     Draggable.create(this.dragWrapper, {
       type: "x",
       trigger: this.cardsList,
       onPress: this.onPress.bind(this),
       onDrag: this.onDrag.bind(this),
       onDragEnd: () => this.scrollToOffset(this.scrub.vars.offset),
+    });
+  }
+
+  setupInitialAnimations() {
+    gsap.set(this.cards, { x: "400%", opacity: 0, scale: 0 });
+    gsap.from(this.sectionWrapper, { autoAlpha: 0, duration: 1, ease: "expo" });
+
+    this.scrub = gsap.to(this.playhead, {
+      offset: 0,
+      onUpdate: this.onScrubUpdate.bind(this),
+      duration: 0.5,
+      ease: "power3",
+      paused: true,
     });
   }
 
@@ -275,7 +282,6 @@ class GalleryScroller {
       { time: cycleDuration + dur / 2 },
       { time: "+=" + cycleDuration, duration: cycleDuration, ease: "none" }
     );
-    console.log(seamlessLoop);
     return seamlessLoop;
   }
 
@@ -291,9 +297,12 @@ class GalleryScroller {
     } else if (scroll < 1 && self.direction < 0) {
       this.wrap(-1, self.end - 2);
     } else {
-      this.scrub.vars.offset =
+      const offset =
         (this.iteration + self.progress) * this.seamlessLoop.duration();
-      this.scrub.invalidate().restart();
+      if (offset !== this.scrub.vars.offset) {
+        this.scrub.vars.offset = offset;
+        this.scrub.invalidate().restart();
+      }
     }
   }
 
@@ -334,165 +343,6 @@ class GalleryScroller {
     this.scrub.vars.offset = this.startOffset + (this.startX - this.x) * 0.001;
     this.scrub.invalidate().restart();
   }
-}
-function gallery() {
-  let iteration = 0; // gets iterated when we scroll all the way to the end or start and wraps around - allows us to smoothly continue the playhead scrubbing in the correct direction.
-
-  // set initial state of items
-  gsap.set(".works-list-item", { xPercent: 400, opacity: 0, scale: 0 });
-  gsap.from("[visibility-hidden]", {
-    autoAlpha: 0,
-    duration: 1,
-    ease: "expo",
-  });
-
-  const spacing = 0.15, // spacing of the cards (stagger)
-    snapTime = gsap.utils.snap(spacing), // we'll use this to snapTime the playhead on the seamlessLoop
-    cards = gsap.utils.toArray(".works-list-item"),
-    // this function will get called for each element in the buildSeamlessLoop() function, and we just need to return an animation that'll get inserted into a master timeline, spaced
-    animateFunc = (element) => {
-      const tl = gsap.timeline();
-      tl.fromTo(
-        element,
-        { scale: 0, opacity: 0 },
-        {
-          scale: 1,
-          opacity: 1,
-          zIndex: 100,
-          duration: 0.5,
-          yoyo: true,
-          repeat: 1,
-          ease: "power1.in",
-          immediateRender: false,
-        }
-      ).fromTo(
-        element,
-        { xPercent: 400 },
-        { xPercent: -400, duration: 1, ease: "none", immediateRender: false },
-        0
-      );
-      return tl;
-    },
-    seamlessLoop = buildSeamlessLoop(cards, spacing, animateFunc),
-    playhead = { offset: 0 }, // a proxy object we use to simulate the playhead position, but it can go infinitely in either direction and we'll just use an onUpdate to convert it to the corresponding time on the seamlessLoop timeline.
-    wrapTime = gsap.utils.wrap(0, seamlessLoop.duration()), // feed in any offset (time) and it'll return the corresponding wrapped time (a safe value between 0 and the seamlessLoop's duration)
-    scrub = gsap.to(playhead, {
-      // we reuse this tween to smoothly scrub the playhead on the seamlessLoop
-      offset: 0,
-      onUpdate() {
-        seamlessLoop.time(wrapTime(playhead.offset)); // convert the offset to a "safe" corresponding time on the seamlessLoop timeline
-      },
-      duration: 0.5,
-      ease: "power3",
-      paused: true,
-    }),
-    trigger = ScrollTrigger.create({
-      start: 0,
-      onUpdate(self) {
-        let scroll = self.scroll();
-        if (scroll > self.end - 1) {
-          wrap(1, 2);
-        } else if (scroll < 1 && self.direction < 0) {
-          wrap(-1, self.end - 2);
-        } else {
-          scrub.vars.offset =
-            (iteration + self.progress) * seamlessLoop.duration();
-          scrub.invalidate().restart(); // to improve performance, we just invalidate and restart the same tween. No need for overwrites or creating a new tween on each update.
-        }
-      },
-      end: "+=3000",
-      pin: ".works-list-wrapper",
-    }),
-    // converts a progress value (0-1, but could go outside those bounds when wrapping) into a "safe" scroll value that's at least 1 away from the start or end because we reserve those for sensing when the user scrolls ALL the way up or down, to wrap.
-    progressToScroll = (progress) =>
-      gsap.utils.clamp(
-        1,
-        trigger.end - 1,
-        gsap.utils.wrap(0, 1, progress) * trigger.end
-      ),
-    wrap = (iterationDelta, scrollTo) => {
-      iteration += iterationDelta;
-      trigger.scroll(scrollTo);
-      trigger.update(); // by default, when we trigger.scroll(), it waits 1 tick to update().
-    };
-
-  // when the user stops scrolling, snap to the closest item.
-  ScrollTrigger.addEventListener("scrollEnd", () =>
-    scrollToOffset(scrub.vars.offset)
-  );
-
-  // feed in an offset (like a time on the seamlessLoop timeline, but it can exceed 0 and duration() in either direction; it'll wrap) and it'll set the scroll position accordingly. That'll call the onUpdate() on the trigger if there's a change.
-  function scrollToOffset(offset) {
-    // moves the scroll playhead to the place that corresponds to the totalTime value of the seamlessLoop, and wraps if necessary.
-
-    let snappedTime = snapTime(offset),
-      progress =
-        (snappedTime - seamlessLoop.duration() * iteration) /
-        seamlessLoop.duration(),
-      scroll = progressToScroll(progress);
-    if (progress >= 1 || progress < 0) {
-      return wrap(Math.floor(progress), scroll);
-    }
-    trigger.scroll(scroll);
-  }
-
-  function buildSeamlessLoop(items, spacing, animateFunc) {
-    let rawSequence = gsap.timeline({ paused: true }), // this is where all the "real" animations live
-      seamlessLoop = gsap.timeline({
-        // this merely scrubs the playhead of the rawSequence so that it appears to seamlessly loop
-        paused: true,
-        repeat: -1, // to accommodate infinite scrolling/looping
-        onRepeat() {
-          // works around a super rare edge case bug that's fixed GSAP 3.6.1
-          this._time === this._dur && (this._tTime += this._dur - 0.01);
-        },
-        onReverseComplete() {
-          this.totalTime(this.rawTime() + this.duration() * 100); // seamless looping backwards
-        },
-      }),
-      cycleDuration = spacing * items.length,
-      dur; // the duration of just one animateFunc() (we'll populate it in the .forEach() below...
-
-    // loop through 3 times so we can have an extra cycle at the start and end - we'll scrub the playhead only on the 2nd cycle
-    items
-      .concat(items)
-      .concat(items)
-      .forEach((item, i) => {
-        let anim = animateFunc(items[i % items.length]);
-        rawSequence.add(anim, i * spacing);
-        dur || (dur = anim.duration());
-      });
-
-    // animate the playhead linearly from the start of the 2nd cycle to its end (so we'll have one "extra" cycle at the beginning and end)
-    seamlessLoop.fromTo(
-      rawSequence,
-      {
-        time: cycleDuration + dur / 2,
-      },
-      {
-        time: "+=" + cycleDuration,
-        duration: cycleDuration,
-        ease: "none",
-      }
-    );
-    return seamlessLoop;
-  }
-
-  //dragging functionality (mobile-friendly too)...
-  Draggable.create(".drag-proxy", {
-    type: "x",
-    trigger: ".works-list",
-    onPress() {
-      this.startOffset = scrub.vars.offset;
-    },
-    onDrag() {
-      scrub.vars.offset = this.startOffset + (this.startX - this.x) * 0.001;
-      scrub.invalidate().restart(); // same thing as we do in the ScrollTrigger's onUpdate
-    },
-    onDragEnd() {
-      scrollToOffset(scrub.vars.offset);
-    },
-  });
 }
 
 class SectionNavigator {
@@ -607,14 +457,14 @@ class SectionNavigator {
   }
 }
 
-barba.hooks.enter((data) => {
-  window.scroll(0, 0);
+barba.hooks.beforeLeave((data) => {
+  Observer.getAll().forEach((o) => o.kill());
+  ScrollTrigger.getAll().forEach((t) => t.kill());
+  //window.scroll(0, 0);
   if (history.scrollRestoration) {
-    history.scrollRestoration = "manual";
+    //history.scrollRestoration = "manual";
   }
 });
-
-let run = true;
 
 barba.init({
   preventRunning: true,
@@ -650,8 +500,8 @@ barba.init({
       namespace: "work-category",
       beforeEnter(data) {
         let nextContainer = data.next.container;
+        Splitting();
         new GalleryScroller(nextContainer);
-        //gallery();
         new Nav(nextContainer);
       },
     },
@@ -665,6 +515,13 @@ barba.init({
         new SectionNavigator(nextContainer);
       },
     },
+    {
+      namespace: "archive",
+      beforeEnter(data) {
+        let nextContainer = data.next.container;
+        new Nav(nextContainer);
+      },
+    },
   ],
   transitions: [
     {
@@ -672,9 +529,7 @@ barba.init({
       enter(data) {
         let nextContainer = data.next.container;
         let currentContainer = data.current.container;
-        //nextContainer.classList.add("fixed");
         gsap.set(currentContainer, { opacity: 0.4, duration: 1 });
-        //window.scroll(0, 0);
         return gsap.fromTo(
           nextContainer,
           {
@@ -689,13 +544,11 @@ barba.init({
       },
       after(data) {
         let nextContainer = data.next.container;
-        //Scrolls to the top of document
-        //nextContainer.classList.remove("fixed");
       },
     },
   ],
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-  gsap.to("body", { autoAlpha: 1, duration: 0.1, ease: "linear" });
+  gsap.from("body", { autoAlpha: 0, duration: 1, ease: "linear" });
 });
